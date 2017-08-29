4372b26f5cb71f05095e8222d9386d503daa8127
Brandon Neri <arcaniteamp@gmail.com>
2017-08-29 11:03:22 -0500

    Instalar moment

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const moment = require('moment');
 const PORT = process.env.PORT || 1337;
 const app = express();
 
 const request = require('request');
-const API_CAPAC = 'http://107.170.200.227/api/v1';
+const API_CAPAC = 'http://104.236.144.72/api/v1';
 
 // Config
 app.set('view engine', 'pug');
@@ -18,8 +19,21 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/blog', (req, res) => {
+  request.get(`${API_CAPAC}/entrada`, (err, response, body) => {
+    if(err) return res.render('index', {evento: {err: 'No se pudieron cargar las entradas del blog.'}});
+    let entradas = JSON.parse(body).reverse();
+    return res.render('blog', {entradas: entradas.map(e => {
+      let temp = e;
+      temp.createdAt = moment(e.createdAt).format('DD/MM/YYYY');
+      return temp;
+    })});
+  });
+})
+
 app.get('/nosotros', (req, res) => res.render('nosotros'));
 app.get('/servicios', (req, res) => res.render('servicios'));
+app.get('/contacto', (req, res) => res.render('contacto'));
 
 // Listen
 app.listen(PORT, () => {
