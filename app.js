c7f7b924411dcbef0babc9723ba2ac2a9ae7fb11
SupineDread <hazielfe@gmail.com>
2017-08-31 13:26:05 -0500

    port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const moment = require('moment');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 1337;
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 const nodemailer = require('nodemailer');
